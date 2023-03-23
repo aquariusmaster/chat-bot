30ffdd0165fe831931f5c81c7a22eaa6e727ee00
Andrii Bobrov <verydrybeaver@gmail.com>
2023-03-23 17:08:22 +0200

    Add try/catch

--- src/main/java/com/anderb/chatbot/DynamoDbChatHistoryClient.java
+++ src/main/java/com/anderb/chatbot/DynamoDbChatHistoryClient.java
@@ -6,35 +6,47 @@ import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.dynamodbv2.document.Table;
 import com.anderb.chatbot.model.Message;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.*;
 import java.util.stream.Collectors;
 
+@Slf4j
 public class DynamoDbChatHistoryClient {
 
     private static final AmazonDynamoDB DYNAMO_DB = AmazonDynamoDBClientBuilder.standard().build();
     private static final DynamoDB DB = new DynamoDB(DYNAMO_DB);
 
     public static List<Message> getChatMessages(Long chatId) {
-        Table table = DB.getTable(Config.DYNAMO_TABLE_NAME);
-        Item chat = table.getItem("chat_id", chatId);
-        return Optional.ofNullable(chat)
-                .map(item -> item.getList("messages"))
-                .stream()
-                .flatMap(Collection::stream)
-                .map(DynamoDbChatHistoryClient::toMessage)
-                .collect(Collectors.toList());
+        log.debug("Getting chat#{} history", chatId);
+        try {
+            Table table = DB.getTable(Config.DYNAMO_TABLE_NAME);
+            Item chat = table.getItem("chat_id", chatId);
+            return Optional.ofNullable(chat)
+                    .map(item -> item.getList("messages"))
+                    .stream()
+                    .flatMap(Collection::stream)
+                    .map(DynamoDbChatHistoryClient::toMessage)
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            log.error("Cannot get chat#{} history", chatId, e);
+            return Collections.emptyList();
+        }
     }
 
     public static void putChatSession(Long chatId, List<Message> messages) {
-        List<Map<String, String>> stringMessages = messages.stream()
-                .map(message -> Map.of("role", message.getRole(), "content", message.getContent()))
-                .collect(Collectors.toList());
-        Item item = new Item()
-                .withPrimaryKey("chat_id", chatId)
-                .withList("messages", stringMessages);
-        Table table = DB.getTable(Config.DYNAMO_TABLE_NAME);
-        table.putItem(item);
+        try {
+            List<Map<String, String>> stringMessages = messages.stream()
+                    .map(message -> Map.of("role", message.getRole(), "content", message.getContent()))
+                    .collect(Collectors.toList());
+            Item item = new Item()
+                    .withPrimaryKey("chat_id", chatId)
+                    .withList("messages", stringMessages);
+            Table table = DB.getTable(Config.DYNAMO_TABLE_NAME);
+            table.putItem(item);
+        } catch (Exception e) {
+            log.error("Cannot update chat#{} history", chatId, e);
+        }
     }
 
     @SuppressWarnings("unchecked")
